48facc1787cb6e24f5ed51f19e36f174aa7e671d
Karan Sharma <sharmakaran0212@gmail.com>
2023-12-04 12:24:23 +0200

    Completed Week 8 Lab 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import Toolbar2 from './Toolbar2';
+import Toolbar from './Toolbar';
+import React from 'react';
+import Button2 from './Button2';
+import Button from './Button'; // Import the Button component
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Passing Event Handlers as Props</h1>
+      <Toolbar2 />
+        <h1>Reading Props in Event Handlers</h1>
+      <Button2 /> {/* Use the Button component */}
+      <Toolbar />
+        <h1>React Events Lab</h1>
+      <Button /> {/* Use the Button component */}
     </div>
   );
 }
 
 export default App;
+
